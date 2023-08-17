d4a1e677b070fd7c9276b36391440fdfe52c1094
Ann Yakubovskaya <a.yakubovskaya@ngrsoftlab.ru>
2023-08-17 15:58:19 +0300

    Oh no

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -7,10 +7,16 @@ import (
 
 func TestMath(t *testing.T) {
 	expression := "2+2-3-5+1"
+
 	p := mathparse.NewParser(expression)
+	
 	if p.FoundResult() {
 		var result float64
 		result = p.GetValueResult()
 		t.Log(result)
+	} else {
+		var expression string
+		expression = p.GetExpressionResult()
+		t.Log(expression)
 	}
 }
